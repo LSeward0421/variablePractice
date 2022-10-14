de7eda7cde4367b496e4afbe22326797266f010d
Lauren Seward <lauren.unal@gmail.com>
2022-10-14 15:53:18 -0500

    Fix deprecation from var name to alias

--- variables.js
+++ variables.js
@@ -1,6 +1,11 @@
 var address = "1212 Practice Ln, Practiceville, Earth"
-var name = "Practice McPracterson"
+var alias = "Practice McPracterson"
 var laughs = 6700.94
 var kittens = 100
 var weatherNice = true
 var halloweenSucks = false
+
+address = "1313 Wrong Ave, Wrongville, Earth"
+alias = 'Wrongy McWrongerson'
+
+
