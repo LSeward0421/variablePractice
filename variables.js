43a89377e6ac95cfd4bfcda5a202d7fd1c93a45a
Lauren Seward <lauren.unal@gmail.com>
2022-10-14 15:43:24 -0500

    Add practice boolean var

--- variables.js
+++ variables.js
@@ -2,3 +2,5 @@ var address = "1212 Practice Ln, Practiceville, Earth"
 var name = "Practice McPracterson"
 var laughs = 6700.94
 var kittens = 100
+var weatherNice = true
+var halloweenSucks = false
